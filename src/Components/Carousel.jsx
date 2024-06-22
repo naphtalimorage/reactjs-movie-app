8485d8893e3acde195d229f06ff0780af7d26c03
Naphtali Morage <naphtalimorage56@gmail.com>
2024-06-22 13:38:42 +0300

    map over film at approximately 5

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,45 +1,65 @@
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+// import React from 'react'
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import './Carousel.css';
 
-const Carousel = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,           
-        autoplaySpeed: 2000,       
-        pauseOnHover: true,       
-        cssEase: 'linear'          
-    };
 
-    const images = [
-        { src: "movie1.png", alt: "movie1" },
-        { src: "movie2.png", alt: "movie2" },
-        { src: "movie3.png", alt: "movie3" },
-        { src: "movie4.png", alt: "movie4" },
-        { src: "movie5.png", alt: "movie5" }
-    ];
 
-    const imageStyle = {
-      width: '100vh',
-      height: '500px',
-      objectfit: 'cover', 
-      margin: '20px' ,
-    };
+// const Carousel = ({movies}) => {
+//     const settings = {
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//       };
 
-    return (
-        <Slider {...settings}>
-            {images.map((image, index) => (
-                <div key={index}>
-                    <img style={imageStyle} src={image.src} alt={image.alt} onError={(e) => e.target.src = 'fallback-image.jpeg'} />
-                </div>
-            ))}
-        </Slider>
-    );
+
+//   return (
+//     <div className='carousel-container'>
+//       <Slider {...settings}>
+//         {movies.slice(0,5).map(movie => {
+//             return (
+//               <div  key={movie.id} className="movieImage">
+//               <img  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.title} poster`} />
+//               </div>
+//             );
+//         })}
+
+//       </Slider>
+//     </div>
+//   )
+// }
+
+// export default Carousel;
+
+import React from "react";
+import "./Carousel.css";
+
+const Carousel = ({ movies }) => {
+  return (
+    <div className="carousel">
+      {movies.map((movie) => (
+        <div
+          className="carousel-item"
+          key={movie.id}
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`,
+          }}
+        >
+          <div className="overlay"></div>
+          <div className="carousel-caption">
+            <h2>{movie.title}</h2>
+            <p>{movie.overview}</p>
+          </div>
+        </div>
+      )  )}
+    </div>
+  );
 };
 
 export default Carousel;
+
