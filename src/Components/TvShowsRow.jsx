bb7c9fe9bc2bdc5ab74e052e48f37d1cf525d315
Naphtali Morage <naphtalimorage56@gmail.com>
2024-06-22 12:31:42 +0300

    map over Film for tvshows

---
+++ src/Components/TvShowsRow.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './MovieRow.css';
+import Film from "./Film";
+
+const TvShowsRow = ({ category, tvshows }) => {
+  return (
+    <div className="movie-row-container">
+      <h2>{category}</h2>
+      <div className='movie-row'>
+        {tvshows.map((tvshow) => (
+          <div  className='movie-item' key={tvshow.id}>
+            <Film  filmId={tvshow.id} filmTitle={tvshow.name} rating={tvshow.vote_average} releaseDate={tvshow.first_air_date} posterUrl={tvshow.poster_path} alt={tvshow.name} filmtype={"tvshow"}/>
+          </div>
+          
+          ))}
+        </div>
+      
+    </div>
+  );
+};
+
+export default TvShowsRow;
