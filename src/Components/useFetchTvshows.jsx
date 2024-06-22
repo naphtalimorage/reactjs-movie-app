d6b10317f0bff86a404e08d330a6e3de11a83044
Naphtali Morage <naphtalimorage56@gmail.com>
2024-06-22 12:35:29 +0300

    custom fetch hook for tv shows

---
+++ src/Components/useFetchTvshows.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const useFetchTvshows = (endpoint,API_KEY) => {
+    const [data, setData]= useState(null);
+    const [error , setError]= useState(null);
+    const[loading, setLoading]= useState(true);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+          setError(null);
+          setLoading(true);
+          try {
+            const response = await fetch(`https://api.themoviedb.org/3/tv/${endpoint}?api_key=${API_KEY}`);
+            if (!response.ok) {
+                throw new Error(`Network response was not ok ${response.status}`);
+            }
+            const json = await response.json();
+              setData(json.results);   
+            } catch (error) {
+            setError(error.message);
+          } finally {
+            setLoading(false);
+          }
+        };
+
+        fetchMovies(); 
+    }, [endpoint,API_KEY]);
+
+  return {data,error,loading};
+};
+
+
+export default useFetchTvshows;
